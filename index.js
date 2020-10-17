e64f79b2437c05a5c8932c3b8e179e4f65f0002e
André Almeida <andre_rdo@hotmail.com>
2020-10-17 20:11:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.listen(4000, err => {
+    if (err) {
+        console.log('Ocorreu um erro!')
+    } else {
+        console.log('Servidor iniciado com sucesso!')
+    }
+})
+
+app.get('/', ((req, res) => {
+    res.send('<h1>olaaaaa</h1>')
+}))
+
+app.get('/ola/:nome/:empresa', ((req, res) => {
+    res.send(`<h1>Olá ${req.params.nome}</h1>`)
+}))
